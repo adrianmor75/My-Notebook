d3e583cce20ce79d1957ee9bc99270c9ffb967a9
Adrian <156933783+adrianmor75@users.noreply.github.com>
2024-02-23 21:23:26 -0600

    Update DataScienceEcosystem.py

--- DataScienceEcosystem.py
+++ DataScienceEcosystem.py
@@ -1,77 +1,74 @@
-#!/usr/bin/env python
-# coding: utf-8
+!/usr/bin/env python
+ coding: utf-8
+# Data Science Tool and Ecosystem
 
-# # Data Science Tool and Ecosystem
+ In this Notebook, Data Science and Ecosystem are Summarized. 
+ In this lab it will use the markdown cells in order to summarized the differents tools using in data science and how the 
+ ecosystem works.
 
-# In this Notebook, Data Science and Ecosystem are Summarized. 
-# In this lab it will use the markdown cells in order to summarized the differents tools using in data science and how the 
-# ecosystem works.
+Some of the popular languages that Data Science use are:
+ 1. Phyton
+ 2. R
+ 3. SQL
+ 4. Scala
+ 5. Julia
+ 6. Java
+ 7. C++
 
-# Some of the popular languages that Data Science use are:
-# 1. Phyton
-# 2. R
-# 3. SQL
-# 4. Scala
-# 5. Julia
-# 6. Java
-# 7. C++
+ The most common libraries use in Data Science are:
+ * Panda
+ * Matplotlib
+ * Scikit-learn
+ * TensorFlow
+ * Pytorch
+ * Apach Spark
+ * Numpy
+ * Scala
+   
 
-# The most common libraries use in Data Science are:
-# * Panda
-# * Matplotlib
-# * Scikit-learn
-# * TensorFlow
-# * Pytorch
-# * Apach Spark
-# * Numpy
-# * Scala
-#   
+ ## **Objectives**
+ * Create one column table
+ * Make different exercise to create a Jupyter Notebook
+ * Practice different ways to creat text with markdown cells
+ * Use code cells with arithmetic operations
 
-# ## **Objectives**
-# * Create one column table
-# * Make different exercise to create a Jupyter Notebook
-# * Practice different ways to creat text with markdown cells
-# * Use code cells with arithmetic operations
+ ## Data Science Tools
+ | Data Science Tools |
+ |--------------------|
+ | MySQL|
+ | PostgresSQL|
+ | MongoDB|
+ |Apache AirFlow|
+ |KubeFlow|
 
-# ## Data Science Tools
-# | Data Science Tools |
-# |--------------------|
-# | MySQL|
-# | PostgresSQL|
-# | MongoDB|
-# |Apache AirFlow|
-# |KubeFlow|
+ ### Below are few examples of evaluatin arithmetic expressions in Phyton
 
-# ### Below are few examples of evaluatin arithmetic expressions in Phyton
-
-# In[1]:
-
-
+In[1]
 3+5
 
 
-# In[2]:
+In[2]:
 
 
 -9+8-7-21+38
 
 
-# In[4]:
+
 
 
 -5*2+(-7/17)
 
 
-# #### Exercise 8 Code exercise
+#### Exercise 8 Code exercise
 
-# In[5]:
+In[5]:
 
 
 'This is a simple arithmetic expresion to multiply then add integers'
 (3*4)+5
 
 
-# #### Converting minutes  to hours
+#### Converting minutes  to hours
 
 # In[6]:
 
@@ -80,5 +77,5 @@
 200/60
 
 
-# ## Author 
-# **Itzam Adrián Morales**
+## Author 
+ **Itzam Adrián Morales**
